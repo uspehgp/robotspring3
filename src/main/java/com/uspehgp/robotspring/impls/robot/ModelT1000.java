7e603d9958d8e46d7541a4345090eea14fe94f89
uspehgp <z995959z>
2020-08-29 19:36:39 +0300

    13 add abstract class BaseModel

--- src/main/java/com/uspehgp/robotspring/impls/robot/ModelT1000.java
+++ src/main/java/com/uspehgp/robotspring/impls/robot/ModelT1000.java
@@ -3,15 +3,10 @@ package com.uspehgp.robotspring.impls.robot;
 import com.uspehgp.robotspring.interfaces.Hand;
 import com.uspehgp.robotspring.interfaces.Head;
 import com.uspehgp.robotspring.interfaces.Leg;
-import com.uspehgp.robotspring.interfaces.Robot;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class ModelT1000 implements Robot, InitializingBean, DisposableBean {
-
-	private Hand hand;
-	private Leg leg;
-	private Head head;
+public class ModelT1000 extends BaseModel implements InitializingBean, DisposableBean {
 
 	private String color;
 	private int year;
@@ -21,17 +16,11 @@ public class ModelT1000 implements Robot, InitializingBean, DisposableBean {
 	}
 
 	public ModelT1000(Hand hand, Leg leg, Head head) {
-		super();
-		this.hand = hand;
-		this.leg = leg;
-		this.head = head;
+		super(hand, leg, head);
 	}
 
 	public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean soundEnabled) {
-		super();
-		this.hand = hand;
-		this.leg = leg;
-		this.head = head;
+		super(hand, leg, head);
 		this.color = color;
 		this.year = year;
 		this.soundEnabled = soundEnabled;
@@ -45,9 +34,9 @@ public class ModelT1000 implements Robot, InitializingBean, DisposableBean {
 	}
 	@Override
 	public void action() {
-		head.calc();
-		hand.catchSomething();
-		leg.go();
+		getHead().calc();
+		getHand().catchSomething();
+		getLeg().go();
 		System.out.println("color: " + color);
 		System.out.println("year: " + year);
 		System.out.println("can play sound: " + soundEnabled);
@@ -56,18 +45,6 @@ public class ModelT1000 implements Robot, InitializingBean, DisposableBean {
 	@Override
 	public void dance() {
 		System.out.println("T1000 is dancing!");
-	}
-
-	public void setHand(Hand hand) {
-		this.hand = hand;
-	}
-
-	public void setLeg(Leg leg) {
-		this.leg = leg;
-	}
-
-	public void setHead(Head head) {
-		this.head = head;
 	}
 
 	public void setColor(String color) {
@@ -82,18 +59,6 @@ public class ModelT1000 implements Robot, InitializingBean, DisposableBean {
 		this.soundEnabled = soundEnabled;
 	}
 
-	public Hand getHand() {
-		return hand;
-	}
-
-	public Leg getLeg() {
-		return leg;
-	}
-
-	public Head getHead() {
-		return head;
-	}
-
 	public String getColor() {
 		return color;
 	}
@@ -106,13 +71,6 @@ public class ModelT1000 implements Robot, InitializingBean, DisposableBean {
 		return soundEnabled;
 	}
 
-	private void initObject() {
-		System.out.println("init!");
-	}
-
-	private void destroyObject() {
-		System.out.println("destroy");
-	}
 
 	@Override
 	public void destroy() throws Exception {
